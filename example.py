43b8c3dd479f2687596a5ba9ed2818448f129a22
Alejandro Ramírez C <60363061+FatChicken277@users.noreply.github.com>
2022-04-20 16:35:49 -0500

    More descriptive code

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 from safe import SafeContentAPI
 
-url = 'https://www.chaty.es/uploads/2021/fotos-de-tetas-amateur-1250.jpg'
-safe = SafeContentAPI(url)
+image_url = 'https://www.chaty.es/uploads/2021/fotos-de-tetas-amateur-1250.jpg' # NSFW PHOTO
+safe = SafeContentAPI(image_url)
 print(safe.get_json())
